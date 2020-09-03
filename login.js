8fdddcfe92e71e6c72c50ef52b80e28ad089c828
BC2212 <ngquangnhutquynh@gmail.com>
2020-09-04 01:03:47 +0700

    Tạo file đăng nhập

---
+++ login.js
@@ -0,0 +1,31 @@
+// Đăng nhập lần đầu
+
+const login = require("facebook-chat-api")
+const fs = require("fs")
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const obj = {email:"ngquangnhutquynh@gmail.com", password:"NyN1312"}
+
+login(obj, (err, api)=>{
+    if(err){
+        switch(err.error){
+            case 'login-approval':
+                console.log('Enter code > ')
+                rl.on('line', (line) => {
+                    err.continue(line)
+                    rl.close
+                })
+                break
+            default:
+                console.error(err)
+        }
+        return
+    }
+
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()))
+})
